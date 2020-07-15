a8d4b5ae200a162adc9949b7b5afc5bf5d5620d3
Suelen <araujo.suelenr@gmail.com>
2020-07-15 18:01:48 -0300

    Customers Migration and Model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $setter
+ * @property string $pay
+ * @property float $system_size
+ * @property float $redline
+ * @property float $bill
+ * @property float $financing
+ * @property float $adders
+ * @property float $gross_ppw
+ * @property float $comission
+ * @property float $setter_fee
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property Carbon $deleted_at
+ */
+
 class Customer extends Model
 {
     const CUSTOMERS = [
