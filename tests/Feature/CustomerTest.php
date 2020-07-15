7b1ea10d0ee96264030b579f5c8f068a6a0409d9
suelenr <58312378+suelenr@users.noreply.github.com>
2020-07-15 20:19:46 -0300

    Update CustomerTest.php

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -11,13 +11,12 @@ class CustomerTest extends FeatureTest
     use RefreshDatabase;
 
     /** @test */
-    public function it_should_list_alll_customers_on_dashboard()
+    public function it_should_list_all_customers_on_dashboard()
     {
         $this->withoutExceptionHandling();
         $customers = factory(Customer::class, 5)->create();
         
         $response = $this->get('/');
-        $response->assertStatus(200);
 
         $response->assertStatus(200);
         $response->assertViewIs('home');
