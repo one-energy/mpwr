8bb01452494d4dac669cb2ffcff54cf806da907c
Ivo Amaral <joseivoamaral@gmail.com>
2020-07-20 22:01:16 -0300

    OE-47 - Created livewire for Number Tracker entry list

--- app/Http/Controllers/NumberTrackingController.php
+++ app/Http/Controllers/NumberTrackingController.php
@@ -29,4 +29,27 @@ class NumberTrackingController extends Controller
 
         return view('number-tracking',compact('showOptions', 'trackingInformation'));
     }
+
+    public function create()
+    {
+        $showOptions = [
+            'Daily Total', 
+            'Weekly Total',
+            'Monthly Total',
+            'Statistics',
+        ];
+        $trackingInformation = [
+            ['team_member' => 'Donna Walker',    'doors' => 100, 'hours' => 9, 'sets' => 8, 'sits' => 2, 'set_closes' => 1, 'closes' => 1],
+            ['team_member' => 'Chris Wiliams',   'doors' => 100, 'hours' => 9, 'sets' => 8, 'sits' => 2, 'set_closes' => 1, 'closes' => 1],
+            ['team_member' => 'Ana Hendersen',   'doors' => 100, 'hours' => 9, 'sets' => 8, 'sits' => 2, 'set_closes' => 1, 'closes' => 1],
+            ['team_member' => 'Donald Barnes',   'doors' => 100, 'hours' => 9, 'sets' => 8, 'sits' => 2, 'set_closes' => 1, 'closes' => 1],
+            ['team_member' => 'Joe Richardson',  'doors' => 100, 'hours' => 9, 'sets' => 8, 'sits' => 2, 'set_closes' => 1, 'closes' => 1],
+            ['team_member' => 'Tammy Collins',   'doors' => 100, 'hours' => 9, 'sets' => 8, 'sits' => 2, 'set_closes' => 1, 'closes' => 1],
+            ['team_member' => 'Joseph Bennett',  'doors' => 100, 'hours' => 9, 'sets' => 8, 'sits' => 2, 'set_closes' => 1, 'closes' => 1],
+            ['team_member' => 'Michelle Powell', 'doors' => 100, 'hours' => 9, 'sets' => 8, 'sits' => 2, 'set_closes' => 1, 'closes' => 1],
+            ['team_member' => 'Jerry Kelly',     'doors' => 100, 'hours' => 9, 'sets' => 8, 'sits' => 2, 'set_closes' => 1, 'closes' => 1],
+            ['team_member' => 'Donna Walker',    'doors' => 100, 'hours' => 9, 'sets' => 8, 'sits' => 2, 'set_closes' => 1, 'closes' => 1],
+        ];
+        return view('number-tracking.create', compact('showOptions', 'trackingInformation'));
+    }
 }
