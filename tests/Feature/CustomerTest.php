e9ffefd0a47a6e37c228f4f4711e0861083cd680
Suelen <araujo.suelenr@gmail.com>
2020-07-23 18:19:36 -0300

    CustomerTest

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -3,24 +3,66 @@
 namespace Tests\Feature;
 
 use App\Models\Customer;
-use Tests\Feature\FeatureTest;
+use App\Models\User;
+use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class CustomerTest extends FeatureTest
+class CustomerTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = factory(User::class)->create();
+        $this->actingAs($this->user);
+    }
 
     /** @test */
     public function it_should_list_all_customers_on_dashboard()
     {
-        $this->withoutExceptionHandling();
         $customers = factory(Customer::class, 5)->create();
         
         $response = $this->get('/');
 
-        $response->assertStatus(200);
-        $response->assertViewIs('home');
-        $response->assertViewHas('customers');
+        $response->assertStatus(200)
+                 ->assertViewIs('home')
+                 ->assertViewHas('customers');
+
         $this->assertEquals($customers->sortBy('name')->pluck('id')->toArray(), $response->viewData('customers')->pluck('id')->toArray());
+    }
+
+    /** @test */
+    public function it_should_filter_by_active_customers()
+    {
+        $activeCustomers   = factory(Customer::class, 3)->create(['is_active' => 1]);
+        $inactiveCustomers = factory(Customer::class, 3)->create(['is_active' => 0]);
+
+        $response = $this->get('/?sort_by=is_active');
+
+        foreach ($activeCustomers as $customers) {
+            $response->assertSee($customers->first_name);
+        }
+
+        foreach ($inactiveCustomers as $customers) {
+            $response->assertDontSee($customers->first_name);
+        }
+    }
+
+    /** @test */
+    public function it_should_filter_by_inactive_customers()
+    {
+        $activeCustomers   = factory(Customer::class, 3)->create(['is_active' => 1]);
+        $inactiveCustomers = factory(Customer::class, 3)->create(['is_active' => 0]);
+
+        $response = $this->get('/?sort_by=is_inactive');
+
+        foreach ($activeCustomers as $customers) {
+            $response->assertDontSee($customers->first_name);
+        }
+
+        foreach ($inactiveCustomers as $customers) {
+            $response->assertSee($customers->first_name);
+        }
     }
 }
