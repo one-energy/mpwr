771ce1be7145c7c024d2eb18869d5b473bea9f12
Ron_Jr <ronaldoj.w2007@gmail.com>
2020-08-19 08:50:06 -0300

    create spinner when change date or region

--- resources/views/livewire/number-tracker/daily-entry.blade.php
+++ resources/views/livewire/number-tracker/daily-entry.blade.php
@@ -106,8 +106,8 @@
                     </div>
                 </div>
 
-                <div class="px-4 py-5 sm:p-6 md:w-2/3">
-                    <div class="flex justify-between mt-3">
+                <div class="flex flex-wrap justify-center px-4 py-5 sm:p-6 md:w-2/3">
+                    <div class="mt-3">
                         <div class="w-full grid md:grid-cols-6 grid-cols-3 md:col-gap-4 col-gap-1 row-gap-2">
                             <div class="col-span-1 border-2 border-gray-200 rounded-lg p-3">
                                 <div class="text-xs font-semibold uppercase">Doors</div>
@@ -274,11 +274,28 @@
                         </div>
                     </div>
 
-                    <div wire:loading class="mt-6">
-                        Loading...
-                    </div>
+                    
+                    <svg class="self-center hidden w-20 mt-3"
+                        wire:loading.class.remove="hidden"
+                        viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#9fa6b2">
+                        <g fill="none">
+                            <g transform="translate(1 1)" stroke-width="2">
+                                <circle stroke-opacity=".5" cx="18" cy="18" r="18" />
+                                <path d="M36 18c0-9.94-8.06-18-18-18">
+                                    <animateTransform
+                                        attributeName="transform"
+                                        type="rotate"
+                                        from="0 18 18"
+                                        to="360 18 18"
+                                        dur="1s"
+                                        repeatCount="indefinite" />
+                                </path>
+                            </g>
+                        </g>
+                    </svg>
+                
 
-                    <div class="mt-3">
+                    <div class="mt-3 w-full">
                         <div class="flex flex-col">
                             <div class="overflow-x-auto">
                                 <div class="align-middle inline-block min-w-full overflow-hidden">
--- resources/views/number-tracking.blade.php
+++ resources/views/number-tracking.blade.php
@@ -439,7 +439,6 @@
                 let selectedDate = new Date(this.year, this.month, date);
                 this.datepickerValue = selectedDate.toDateString();
                 this.$refs.date.value = selectedDate.getFullYear() +"-"+ ('0'+ selectedDate.getMonth()).slice(-2) +"-"+ ('0' + selectedDate.getDate()).slice(-2);
-                console.log(this.$refs.date.value);
                 this.showDatepicker = true;
             },
             getNoOfDays() {
