905567aabff460a44a96b4bc9252932807ed28d5
Ron_Jr <ronaldoj.w2007@gmail.com>
2020-09-15 16:47:38 -0300

    create seeder and factory to training section

---
+++ database/factories/TrainingContentFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\TrainingPageContent;
+use Faker\Generator as Faker;
+
+$factory->define(TrainingPageContent::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
