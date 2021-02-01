4f52332faf983dc917ae5ad57c7406aae91ee8d0
Ron_Jr <ronaldoj.w2007@gmail.com>
2021-02-01 10:55:12 -0300

    change window per self

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
+self.addEventListener('beforeinstallprompt', (e) => {
     // Prevent the mini-infobar from appearing on mobile
     e.preventDefault();
     // Stash the event so it can be triggered later.
@@ -9,7 +9,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
     showInstallPromotion();
 });
 
-window.addEventListener('appinstalled', (evt) => {
+self.addEventListener('appinstalled', (evt) => {
     // Log install to analytics
     console.log('INSTALL: Success');
 });
