6a7a7514457341c64772a5b7dc3126b3a2ada6c6
Ron_Jr <ronaldoj.w2007@gmail.com>
2021-02-01 11:18:43 -0300

    remove button evet

--- public/sw.js
+++ public/sw.js
@@ -13,18 +13,3 @@ self.addEventListener('appinstalled', (evt) => {
     // Log install to analytics
     console.log('INSTALL: Success');
 });
-
-buttonInstall.addEventListener('click', (e) => {
-    // Hide the app provided install promotion
-    hideMyInstallPromotion();
-    // Show the install prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the install prompt');
-      } else {
-        console.log('User dismissed the install prompt');
-      }
-    });
-  });
