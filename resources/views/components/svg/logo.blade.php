accfa5aa2db4b754d8252748f99ed83f79f7d715
Ron_Jr <ronaldoj.w2007@gmail.com>
2021-02-17 18:21:50 -0300

    OE-136 change icon size

--- resources/views/components/svg/logo.blade.php
+++ resources/views/components/svg/logo.blade.php
@@ -1,5 +1,5 @@
 <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
- width="120" viewBox="0 0 2000.000000 750.000000"
+ width="100" viewBox="0 0 2000.000000 750.000000"
  preserveAspectRatio="xMidYMid meet">
 
 <g transform="translate(0.000000,750.000000) scale(0.100000,-0.100000)"
