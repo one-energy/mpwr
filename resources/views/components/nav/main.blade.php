4f04d416a205da76385098a570627f80832776cb
Ron_Jr <ronaldoj.w2007@gmail.com>
2021-02-23 09:26:22 -0300

    add identation

--- resources/views/components/nav/main.blade.php
+++ resources/views/components/nav/main.blade.php
@@ -187,7 +187,8 @@
             <div class="flex items-center justify-between px-4 py-3 sm:px-3">
                 @if(is_active('castle.*'))
                     <x-nav.link-mobile :href="route('home')" class="flex flex-row items-center whitespace-no-wrap">
-                        <x-svg.chevron-left class="w-6 -ml-2"/> <span class="text-xs">@lang('Leave Admin')</span>
+                        <x-svg.chevron-left class="w-6 -ml-2"/>
+                        <span class="text-xs">@lang('Leave Admin')</span>
                     </x-nav.link-mobile>
 
                     <x-nav.link-mobile :href="route('castle.dashboard')" class="mt-1"
