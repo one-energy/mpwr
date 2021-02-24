2de801fa63d4e5d50662a74d5fb5bb5caa86b79e
Ron_Jr <ronaldoj.w2007@gmail.com>
2021-02-24 15:37:47 -0300

    add italic format

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -70,7 +70,7 @@
                                         </div>
                                     </div>
                                     <div class="col-span-7 text-xs text-gray-600">
-                                        {{ number_format($customer->epc) }}kW - {{ date('j F, Y', strtotime($customer->date_of_sale))}}
+                                        {{ number_format($customer->epc) }}kW - <i> {{ date('d/m/Y', strtotime($customer->date_of_sale))}} </i>
                                     </div>
                                 </div>
                             </a>
