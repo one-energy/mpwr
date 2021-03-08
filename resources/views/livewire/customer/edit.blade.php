361c4086cf287392583a9ded4b449a1fc7cee2c7
Ron_Jr <ronaldoj.w2007@gmail.com>
2021-03-08 14:54:46 -0300

    fix code

--- resources/views/livewire/customer/edit.blade.php
+++ resources/views/livewire/customer/edit.blade.php
@@ -182,7 +182,7 @@
                         <div class="mt-3">
                         <span
                             class="block w-full font-bold transition duration-150 ease-in-out sm:text-sm sm:leading-5">
-                            ${{ number_format(floatVal($customer->setter_fee), 2) }}
+                            ${{ number_format(floatval($customer->setter_fee), 2) }}
                         </span>
                         </div>
                     </div>
