916cf8ecd19bea8c49a8eea7603a84a2dc70b138
Lucas Pessoa <lucaspessoa@Lucass-MBP.localdomain>
2021-03-11 10:40:54 -0700

    fix infinite livewire request

--- resources/views/components/input-calendar.blade.php
+++ resources/views/components/input-calendar.blade.php
@@ -118,7 +118,6 @@
                 this.month = day.getMonth();
                 this.year = day.getFullYear();
                 this.datepickerValue = new Date(this.year, this.month, day.getUTCDate()).toDateString();
-                this.getDateValue(day.getUTCDate());
             },
 
             isToday(date) {
