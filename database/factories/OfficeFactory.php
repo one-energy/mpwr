027047d81e21662def1a9f1346c5c88999ea7c65
Ron_Jr <ronaldoj.w2007@gmail.com>
2021-04-01 14:32:19 -0300

    OE-182 fix factory

--- database/factories/OfficeFactory.php
+++ database/factories/OfficeFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Office;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class OfficeFactory extends Factory
--- database/factories/RegionFactory.php
+++ database/factories/RegionFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Region;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class RegionFactory extends Factory
