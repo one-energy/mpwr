5eefd7aa9b0c16bfbd4c05efe1ff4214fce1ac0a
Mathews Lima <mathews.lima@devsquad.com>
2021-04-02 11:55:33 -0300

    QE-184 🔨 fixes factories

--- database/factories/FinancerFactory.php
+++ database/factories/FinancerFactory.php
@@ -2,10 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\Financer;
 use Illuminate\Database\Eloquent\Factories\Factory;
+
 class FinancerFactory extends Factory
 {
     protected $model = Financer::class;
+
     public function definition()
     {
         return [
--- database/factories/RegionFactory.php
+++ database/factories/RegionFactory.php
@@ -2,16 +2,19 @@
 
 namespace Database\Factories;
 
+use App\Models\Region;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class RegionFactory extends Factory
 {
     protected $model = Region::class;
+
     public function definition()
     {
         return [
-            'name' => $this->faker->city,
+            'name'              => $this->faker->city,
+            'region_manager_id' => User::factory(),
         ];
     }
-
 }
