83a772152bd0d07b8c7eff7f610b6039282d37a8
Mathews Lima <mathews.lima@devsquad.com>
2021-04-07 11:29:17 -0300

    OE-204 🔨 campare string instead of carbon

--- app/Services/NumberTrackingService.php
+++ app/Services/NumberTrackingService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\DailyNumber;
 use App\Models\User;
+use Illuminate\Support\Carbon;
 
 final class NumberTrackingService
 {
@@ -11,13 +12,16 @@ final class NumberTrackingService
     {
         $user = User::findOrFail(array_keys($validatedFields['numbers'])[0]);
 
+        /** @var Carbon */
+        $date = $validatedFields['date'];
+
         $filteredNumbers = collect($validatedFields['numbers'][$user->id])
             ->filter(fn ($element) => (int)$element >= 0 || $element !== null);
 
         if ($filteredNumbers->isNotEmpty()) {
             DailyNumber::updateOrCreate([
                 'user_id'   => $user->id,
-                'date'      => $validatedFields['date'],
+                'date'      => $date->toDateString(),
                 'office_id' => $user->office_id,
             ], $filteredNumbers->toArray());
 
