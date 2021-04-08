55b697a1def9139410260cbcc34ec64e4d183fdb
Ron_Jr <ronaldoj.w2007@gmail.com>
2021-04-08 12:34:48 -0300

    OE-225 WIP

--- resources/views/components/drop-file.blade.php
+++ resources/views/components/drop-file.blade.php
@@ -1,9 +1,9 @@
 <div>
     <div class="flex flex-col flex-grow mb-3">
-        <div x-data="{ files: null }" id="FileUpload" class="block w-full py-2 px-3 relative bg-white appearance-none border-2 border-gray-300 border-dashed rounded-md hover:shadow-outline-gray">
+        <div x-data="initDropFileComponent()" id="FileUpload" class="block w-full py-2 px-3 relative bg-white appearance-none border-2 border-gray-300 border-dashed rounded-md">
             <input type="file" multiple
                    class="absolute inset-0 z-50 m-0 p-0 w-full h-full outline-none opacity-0"
-                   x-on:change="files = $event.target.files; console.log($event.target.files);"
+                   x-on:change="addFile($event.target.files)"
                    x-on:dragover="$el.classList.add('active')" x-on:dragleave="$el.classList.remove('active')" x-on:drop="$el.classList.remove('active')"
             >
             <template x-if="files !== null">
@@ -20,13 +20,48 @@
                     </template>
                 </div>
             </template>
-            <template x-if="files === null">
-                <div class="flex flex-col space-y-2 items-center justify-center">
-                    <x-icon class="w-1/3 h-auto" icon="upload-file"></x-icon>
-                    <p class="text-gray-700 text-center">Drag your files here or click in this area.</p>
-                    <a href="javascript:void(0)" class="flex items-center mx-auto py-2 px-4 text-white text-center font-medium border border-transparent rounded-md outline-none bg-green-base">Select a file</a>
-                </div>
-            </template>
+
+            <div class="flex flex-col space-y-2 items-center justify-center">
+                <x-icon class="w-1/3 h-auto" icon="upload-file"></x-icon>
+                <p class="text-gray-700 text-center">Drag your files here or click in this area.</p>
+                <a href="javascript:void(0)" class="flex items-center mx-auto py-2 px-4 text-white text-center font-medium border border-transparent rounded-md outline-none bg-green-base">Select a file</a>
+            </div>
+
         </div>
     </div>
+    @push('scripts')
+        <script>
+            const G_SIZE = 1000000000;
+            const M_SIZE = 1000000;
+            const K_SIZE = 1000;
+            function initDropFileComponent (){
+                return {
+                    files: null,
+                    filesize(fileSize) {
+                        if(fileSize <= K_SIZE){
+                            return fileSize + ' B';
+                        }
+
+                        if(fileSize <= M_SIZE){
+                            return (fileSize/K_SIZE).toFixed(2) + ' KB';
+                        }
+
+                        if(fileSize <= G_SIZE){
+                            return (fileSize/M_SIZE).toFixed(2) + ' MB';
+                        }
+
+                        return (fileSize/G_SIZE).toFixed(2) + ' GB';
+                    },
+                    addFile(files) {
+                        if(!this.files){
+                            console.log(files);
+                            this.files = files;
+                        }else{
+                            this.files.push(files)
+                        }
+                    }
+                }
+            }
+        </script>
+    @endpush
 </div>
