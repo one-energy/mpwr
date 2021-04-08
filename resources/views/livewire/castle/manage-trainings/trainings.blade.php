c3b6c5488845ecfe2c626270bdc6fe27b5f32c40
Mathews Lima <mathews.lima@devsquad.com>
2021-04-08 14:02:31 -0300

    OE-185 move modal to folders component, add videos component

--- resources/views/livewire/castle/manage-trainings/trainings.blade.php
+++ resources/views/livewire/castle/manage-trainings/trainings.blade.php
@@ -218,60 +218,25 @@
 
             <div class="mt-15">
                 @if(!$path || ($content == null && $sections->isEmpty()) )
-                    <div class="h-96 ">
+                    <div class="h-96">
                         <div class="flex justify-center align-middle">
                             <div class="text-sm text-center text-gray-700">
-                            <x-svg.draw.empty></x-svg.draw.empty>
-                            No data yet.
+                                <x-svg.draw.empty></x-svg.draw.empty>
+                                No data yet.
                             </div>
                         </div>
                     </div>
                 @endif
-                {{-- <div class="mt-2 text-lg">
-                    @if($videoId)
-                        <div class="w-full text-center embed-container">
-                            <iframe class="lg:float-right self-center w-full max-w-xl xl:h-80 lg:h-72 lg:w-3/5" src="https://www.youtube.com/embed/{{$videoId}}" frameborder='0' width="500" allowfullscreen></iframe>
-                        </div>
-                    @endif
 
-                    @if($content && $content->title)
-                    <div class="mt-3 text-xl font-semibold">
-                        {{$content->title}}
-                    </div>
-                    <div class="grid" id="reader"></div>
-                    @endif
-                </div> --}}
-                <div class="grid grid-cols-1 gap-y-3 sm:gap-x-3 lg:gap-x-5 md:grid-cols-3">
-                    @foreach($sections as $section)
-                        <livewire:castle.manage-trainings.folder :section="$section" :key="$section->id" />
-                    @endforeach
-                </div>
-                <div x-data="modal()" @on-destroy-section.window="open">
-                    <div x-show="isOpen()" wire:loading.remove class="fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center z-20">
-                        <div x-show="isOpen()" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed inset-0 transition-opacity">
-                            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-                        </div>
-                        <div x-show="isOpen()" @click.away="close" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
-                            <div class="absolute top-0 right-0 pt-4 pr-4">
-                                <button type="button" @click="close" class="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150" aria-label="Close">
-                                    <x-svg.x class="w-5 h-5" />
-                                </button>
-                            </div>
-                            <div class="px-4 py-5 sm:p-6">
-                                <h3 class="text-left" x-text="`Do you want to delete ${section.title} section?`"></h3>
-                                <p class="text-left mt-4 text-base text-gray-700" x-text="`If you delete this section all content into ${section.title} will be pass to {{$actualSection->title}}`"></p>
-                                <div class="mr-4 mb-4 inline-flex space-x-4 float-right mt-8">
-                                    <x-button class="w-full flex ml-4" @click="close()">
-                                        {{ __('No') }}
-                                    </x-button>
-                                    <x-form :route="$sectionDestroyRoute" delete x-data="{deleting: false}">
-                                        <x-button class="w-full flex ml-4" type="submit" color="green">
-                                            {{ __('Yes') }}
-                                        </x-button>
-                                    </x-form>
-                                </div>
-                            </div>
-                        </div>
+                <livewire:castle.manage-trainings.folders :currentSection="$actualSection" :sections="$sections" />
+
+                <div class="mt-10">
+                    <h3 class="text-xl text-cool-gray-700 font-medium mb-3.5">Videos</h3>
+
+                    <div class="grid grid-cols-1 gap-y-3 sm:gap-x-3 lg:gap-x-5 md:grid-cols-2 xl:grid-cols-3">
+                        @foreach ([1, 2, 3, 4, 5] as $item)
+                            <livewire:castle.manage-trainings.videos :videoId="$videoId" :key="$item" />
+                        @endforeach
                     </div>
                 </div>
             </div>
@@ -281,17 +246,6 @@
 
 
 <script>
-    const modal = () => ({
-        section: {},
-        show: false,
-        open(event) {
-            this.section = {...event.detail.section};
-            this.show = true;
-        },
-        close() { this.show = false },
-        isOpen() { return this.show === true },
-    })
-
   var readerOption = {
     readOnly: true
   }
