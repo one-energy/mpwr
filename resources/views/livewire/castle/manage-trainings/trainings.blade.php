1753ea6dec0aa968fa5cbc419a2a847fc954f1d8
Mathews Lima <mathews.lima@devsquad.com>
2021-04-11 13:55:59 -0300

    OE-185 show or hide contents based on selected tab

--- resources/views/livewire/castle/manage-trainings/trainings.blade.php
+++ resources/views/livewire/castle/manage-trainings/trainings.blade.php
@@ -1,21 +1,39 @@
 <div>
     <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
         <div class="flex justify-center mt-7 mb-5">
-            <button class="py-2 focus:outline-none rounded-l shadow-md w-96" style="background-color: #F1F1F1">Files</button>
-            <button class="py-2 focus:outline-none rounded-r shadow-md  text-white w-96" style="background-color: #1AAE9F">Training</button>
+            <button class="
+                py-2 focus:outline-none rounded-l shadow-md w-96
+                @if ($this->filesTabSelected)
+                    bg-green-450 text-white
+                @else
+                    bg-gray-base  text-gray-800
+                @endif
+            " wire:click="changeTab('files')">
+                Files
+            </button>
+            <button class="
+                py-2 focus:outline-none rounded-r shadow-md w-96
+                @if ($this->trainingTabSelected)
+                    bg-green-450 text-white
+                @else
+                    bg-gray-base  text-gray-800
+                @endif
+            " wire:click="changeTab('training')">
+                Training
+            </button>
         </div>
 
         <div class="px-4 py-5 sm:px-6">
             <div class="flex flex-col lg:flex-row lg:justify-end">
                 <div class="order-last lg:order-none md:flex-1 xl:ml-1/6">
                     @foreach($path as $pathSection)
-                        <a href="/castle/manage-trainings/list/{{$departmentId}}/{{$pathSection->id}}" class="text-cool-gray-500 align-baseline">{{$pathSection->title}}</a>
-                        <span class="text-cool-gray-500">/</span>
+                        <a href="/castle/manage-trainings/list/{{$departmentId}}/{{$pathSection->id}}" class="text-gray-500 align-baseline">{{$pathSection->title}}</a>
+                        <span class="text-gray-500">/</span>
                     @endforeach
                     @if(user()->role == "Admin" || user()->role == "Owner" || user()->role == "Department Manager")
                         <div class="inline-flex" x-data="{ 'editSectionModal': false }" @keydown.escape="editSectionModal = false" x-cloak>
-                            <button class="p-3 rounded-full  hover:bg-gray-200" @click="editSectionModal = true">
-                                <x-svg.pencil class="w-4 h-4 fill-current text-cool-gray-800" />
+                            <button class="p-3 rounded-full  hover:bg-gray-100" @click="editSectionModal = true">
+                                <x-svg.pencil class="w-4 h-4 fill-current text-gray-800" />
                             </button>
                             <div x-show="editSectionModal" wire:loading.remove class="fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center z-20">
                                 <div x-show="editSectionModal" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed inset-0 transition-opacity">
@@ -58,7 +76,7 @@
                 <div class="flex justify-center lg:justify-end mb-3.5 lg:mr-6">
                     @if(user()->role == "Admin" || user()->role == "Owner" || user()->role == "Department Manager")
                         <div class="mr-4" x-data="{ 'showSectionModal': false }" @keydown.escape="showSectionModal = false" x-cloak>
-                            <button style="background-color: #1AAE9F" class="text-white focus:outline-none font-medium text-sm rounded shadow-md px-4 md:px-5 py-2.5" @click="showSectionModal = true">
+                            <button class="bg-green-450 text-white focus:outline-none font-medium text-sm rounded shadow-md px-4 md:px-5 py-2.5" @click="showSectionModal = true">
                                 Add Section
                             </button>
                             <div x-show="showSectionModal" wire:loading.remove class="fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center z-20">
@@ -101,7 +119,7 @@
                         </div>
                         @if(!$contents)
                             <div class="col-span-1" x-data="{ 'showContentModal': false }" @keydown.escape="showContentModal = false" x-cloak>
-                                <button style="background-color: #1AAE9F" class="text-white focus:outline-none font-medium text-sm rounded shadow-md px-4 md:px-5 py-2.5" @click="showContentModal = true">
+                                <button class="bg-green-450 text-white focus:outline-none font-medium text-sm rounded shadow-md px-4 md:px-5 py-2.5" @click="showContentModal = true">
                                     Add Section
                                 </button>
                                 <div x-show="showContentModal" wire:loading.remove class="fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center z-20">
@@ -145,44 +163,55 @@
                             </div>
                         @endif
                         @if($contents)
-                            <div class="col-span-1 sm:col-span-3" x-data="{ 'showContentModal':false }" x-on:keydown.escape="alert($event.target.value)" @keydown.escape="showContentModal = false" x-cloak>
-                                <button style="background-color: #1AAE9F" class="text-white font-medium text-sm rounded shadow-md px-4 md:px-5 py-2.5" @click="showContentModal = true">
-                                    Edit Content
+                            <div class="col-span-1 sm:col-span-3" x-data="addContentHandler()" @content-added.window="alert('aee')"  x-cloak>
+                                <button class="bg-green-450  text-white font-medium text-sm rounded shadow-md px-4 md:px-5 py-2.5" @click="open">
+                                    Add Content
                                 </button>
 
-                                <div x-show="showContentModal" wire:loading.remove class="fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center z-20">
-                                    <div x-show="showContentModal" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed inset-0 transition-opacity">
+                                <div x-show="show" wire:loading.remove class="fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center z-20" x-clock>
+                                    <div x-show="show" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed inset-0 transition-opacity">
                                         <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
                                     </div>
-                                    <div x-show="showContentModal" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" class="relative bottom-16 p-4 overflow-y-auto max-h-96 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
+                                    <div
+                                        @click.away="close" x-show="show" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                        class="relative bottom-16 p-4 overflow-y-auto bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full"
+                                        x-cloak
+                                    >
                                         <div class="absolute top-0 right-0 pt-4 pr-4">
-                                            <button type="button" x-on:click="showContentModal = false; setTimeout(() => open = true, 1000)" class="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150" aria-label="Close">
-                                                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                                                </svg>
+                                            <button type="button" @click="close" class="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150" aria-label="Close">
+                                                <x-svg.x class="w-5 h-5" />
                                             </button>
                                         </div>
                                         <div class="sm:p-6">
-                                            <div class="flex justify-between">
-                                                <div class="flex justify-start">
-                                                    <div class="inline-grid items-center">
-                                                        {{-- <h3>Edit the content to {{$actualSection->title}}</h3> --}}
-                                                        <x-form id="formContent" route="">
-                                                            <div class="grid grid-cols-2 mt-8 mb-4 gap-2">
-                                                                {{-- <x-input class="col-span-1" label="Title" name="content_title" value="{{$content->title}}"/> --}}
-                                                                {{-- <x-input class="col-span-1" label="Video Url" name="video_url" value="{{$content->video_url}}"/> --}}
-                                                                {{-- <x-text-area id="description" class="col-span-2" label="Description" name="description" value="{{$content->description}}" hidden></x-text-area> --}}
-                                                            </div>
-                                                            <div class="grid" id="editor"></div>
-                                                            <div class="mt-6">
-                                                                <span class="block w-full rounded-md shadow-sm">
-                                                                    <x-button class="w-full flex" type="submit" color="green">
-                                                                        {{ __('Save') }}
-                                                                    </x-button>
-                                                                </span>
-                                                            </div>
-                                                        </x-form>
-                                                    </div>
+                                            <div class="flex my-4">
+                                                <button
+                                                    class="py-2 focus:outline-none rounded-l shadow-md w-96"
+                                                    :class="tabVideoSelected ? activeTabColors : inactiveTabColors"
+                                                    @click="changeTab('video')"
+                                                >
+                                                    Video
+                                                </button>
+                                                <button
+                                                    class="py-2 focus:outline-none rounded-r shadow-md w-96"
+                                                    :class="tabFileSelected ? activeTabColors : inactiveTabColors"
+                                                    @click="changeTab('file')"
+                                                >
+                                                    File
+                                                </button>
+                                            </div>
+
+                                            <div x-show="tabVideoSelected">
+                                                <div class="grid grid-cols-2 mt-8 gap-2 mb-4">
+                                                    <x-input class="col-span-1" label="Title" wire:model.defer="video.title" name="video.title" />
+                                                    <x-input class="col-span-1" label="Video Url" wire:model.defer="video.video_url" name="video.video_url"/>
+                                                    <x-text-area class="col-span-2" label="Description" wire:model.defer="video.description" name="video.description"></x-text-area>
+                                                </div>
+                                                <div class="mt-6">
+                                                    <span class="block w-full rounded-md shadow-sm">
+                                                        <x-button class="w-full flex" type="submit" color="green" @click="$wire.storeVideo()">
+                                                            {{ __('Save') }}
+                                                        </x-button>
+                                                    </span>
                                                 </div>
                                             </div>
                                         </div>
@@ -232,10 +261,44 @@
                     <livewire:castle.manage-trainings.folders :currentSection="$actualSection" :sections="$sections" />
                 </div>
 
-                <div class="mt-10">
-                    <livewire:castle.manage-trainings.videos :contents="$contents" />
+                <div class="mt-10 @if ($this->filesTabSelected) hidden @endif">
+                    <livewire:castle.manage-trainings.videos :currentSection="$actualSection" :contents="$contents" />
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    const addContentHandler = () => ({
+        show: false,
+        selectedTab: 'video',
+        changeTab(tab) {
+            this.selectedTab = tab;
+        },
+        open() {
+            setTimeout(() => {
+                this.show = true;
+            }, 100);
+        },
+        close() {
+            this.show = false;
+        },
+        get tabVideoSelected() {
+            return this.selectedTab === 'video';
+        },
+        get tabFileSelected() {
+            return this.selectedTab === 'file';
+        },
+        get activeTabColors() {
+            return 'bg-green-450 text-white';
+        },
+        get inactiveTabColors() {
+            return 'bg-gray-300 text-gray-800';
+        },
+    });
+
+    window.addEventListener('name-updated', event => {
+    alert('Name updated to: ' + event.detail.newName);
+})
+</script>
