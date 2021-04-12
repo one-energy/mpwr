ba5db6978cf8a2ccc4c105ae0b6629c0d89a66a0
Mathews Lima <mathews.lima@devsquad.com>
2021-04-12 08:32:18 -0300

    OE-185 🔒️ new test cases for trainings component

--- tests/Feature/Livewire/Castle/ManageTrainings/TrainingsTest.php
+++ tests/Feature/Livewire/Castle/ManageTrainings/TrainingsTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Livewire\Castle\ManageTrainings;
 
 use App\Http\Livewire\Castle\ManageTrainings\Trainings;
 use App\Models\Department;
+use App\Models\TrainingPageContent;
 use App\Models\TrainingPageSection;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -30,5 +31,90 @@ class TrainingsTest extends TestCase
             ->call('changeTab', 'training')
             ->assertSet('filesTabSelected', false)
             ->assertSet('trainingTabSelected', true);
+    }
+
+    /** @test */
+    public function it_should_be_possible_store_a_video()
+    {
+        $john = User::factory()->create(['role' => 'Admin']);
+
+        $department = Department::factory()->create();
+        $section    = TrainingPageSection::factory()->create(['department_id' => $department->id]);
+
+        $this->actingAs($john);
+
+        $this->assertDatabaseCount('training_page_contents', TrainingPageContent::count());
+
+        Livewire::test(Trainings::class, ['department' => $department])
+            ->set('video.title', 'title')
+            ->set('video.video_url', 'url')
+            ->set('video.description', 'description')
+            ->call('storeVideo')
+            ->assertHasNoErrors();
+
+        $this->assertDatabaseCount('training_page_contents', TrainingPageContent::count());
+        $this->assertDatabaseHas('training_page_contents', [
+            'title'                    => 'title',
+            'video_url'                => 'url',
+            'description'              => 'description',
+            'training_page_section_id' => $section->id,
+        ]);
+    }
+
+    /** @test */
+    public function it_should_require_title_to_store_video()
+    {
+        $john = User::factory()->create(['role' => 'Admin']);
+
+        $department = Department::factory()->create();
+        TrainingPageSection::factory()->create(['department_id' => $department->id]);
+
+        $this->actingAs($john);
+
+        $this->assertDatabaseCount('training_page_contents', TrainingPageContent::count());
+
+        Livewire::test(Trainings::class, ['department' => $department])
+            ->set('video.video_url', 'url')
+            ->set('video.description', 'description')
+            ->call('storeVideo')
+            ->assertHasErrors(['video.title' => 'required']);
+    }
+
+    /** @test */
+    public function it_should_require_video_url_to_store_video()
+    {
+        $john = User::factory()->create(['role' => 'Admin']);
+
+        $department = Department::factory()->create();
+        TrainingPageSection::factory()->create(['department_id' => $department->id]);
+
+        $this->actingAs($john);
+
+        $this->assertDatabaseCount('training_page_contents', TrainingPageContent::count());
+
+        Livewire::test(Trainings::class, ['department' => $department])
+            ->set('video.title', 'title')
+            ->set('video.description', 'description')
+            ->call('storeVideo')
+            ->assertHasErrors(['video.video_url' => 'required']);
+    }
+
+    /** @test */
+    public function it_should_require_description_to_store_video()
+    {
+        $john = User::factory()->create(['role' => 'Admin']);
+
+        $department = Department::factory()->create();
+        TrainingPageSection::factory()->create(['department_id' => $department->id]);
+
+        $this->actingAs($john);
+
+        $this->assertDatabaseCount('training_page_contents', TrainingPageContent::count());
+
+        Livewire::test(Trainings::class, ['department' => $department])
+            ->set('video.title', 'title')
+            ->set('video.video_url', 'url')
+            ->call('storeVideo')
+            ->assertHasErrors(['video.description' => 'required']);
     }
 }
