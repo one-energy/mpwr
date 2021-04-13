01ed4c669a4e534ea935b206493dc461bfac270b
Mathews Lima <mathews.lima@devsquad.com>
2021-04-13 10:16:18 -0300

    OE-185 rename files to match with Model

--- database/migrations/2021_04_07_201324_create_section_files_table.php
+++ database/migrations/2021_04_07_201324_create_section_files_table.php
@@ -4,18 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFilesTable extends Migration
+class CreateSectionFilesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('section_files', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('training_page_section_id')->cascade();
+            $table->foreignId('training_page_section_id')->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->string('original_name');
             $table->string('type');
@@ -25,11 +20,6 @@ class CreateFilesTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('section_files');
