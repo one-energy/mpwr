96a82e96bd67783ec6bd1d8d753b764038a614c3
Mathews Lima <mathews.lima@devsquad.com>
2021-04-13 11:30:50 -0300

    OE-185 add cursor-pointer, fix date format

--- resources/views/livewire/list-files.blade.php
+++ resources/views/livewire/list-files.blade.php
@@ -16,15 +16,14 @@
         <x-slot name="body">
             @foreach ($files as $file)
                 <x-table.tr :loop="$loop">
-                    <x-table.td class="flex align-middle" wire:click="downloadSectionFile({{$file}})">
+                    <x-table.td class="flex align-middle cursor-pointer" wire:click="downloadSectionFile({{$file}})">
                         <x-icon class="h-5 w-auto mr-5" icon="download-file"/>
                         {{ $file->original_name }}
                     </x-table.td>
                     <x-table.td>{{ $file->abbreviatedSize }}</x-table.td>
-                    <x-table.td>{{ $file->created_at->format('d/m/Y') }} at {{$file->created_at->format('h:ia')}}</x-table.td>
+                    <x-table.td>{{ $file->created_at->format('m/d/Y') }} at {{$file->created_at->format('h:ia')}}</x-table.td>
                     <x-table.td wire:click="removeFile({{$file}})">
-                        <button class="hover:bg-red-200 focus:outline-none p-2 rounded-full">
-                            {{-- <x-icon icon="trash"/> --}}
+                        <button class="hover:bg-red-200 focus:outline-none p-2 rounded-full cursor-pointer">
                             <x-svg.trash class="w-5 h-5  text-red-600 fill-current" />
                         </button>
                     </x-table.td>
