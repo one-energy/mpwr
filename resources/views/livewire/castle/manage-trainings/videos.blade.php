12fb27786cad3830a1ad7eed1568cb0cfc7a2777
Mathews Lima <mathews.lima@devsquad.com>
2021-04-13 11:31:06 -0300

    OE-185 show videos if exists some content

--- resources/views/livewire/castle/manage-trainings/videos.blade.php
+++ resources/views/livewire/castle/manage-trainings/videos.blade.php
@@ -1,39 +1,41 @@
 <div>
-    <h3 class="text-xl text-gray-700 font-medium mb-3.5">Videos</h3>
+    @if ($contents->isNotEmpty())
+        <h3 class="text-xl text-gray-700 font-medium mb-3.5">Videos</h3>
 
-    <div class="grid grid-cols-1 gap-y-3 sm:gap-x-3 lg:gap-x-5 md:grid-cols-2 xl:grid-cols-3">
-        @foreach ($contents as $content)
-            <div wire:key="video-{{ $content->id }}" class="grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-5 lg:gap-x-3 border-2 border-gray-300 rounded p-4">
-                <div class="col-span-full lg:col-span-2 relative bg-gray-800 cursor-pointer" wire:click="openShowVideoModal({{ $content->id }})">
-                    <iframe class="absolute inset-0 w-full h-full pointer-events-none" src="{{ $this->makeVideoUrl($content) }}" frameborder="0"></iframe>
-                </div>
-                <div class="lg:col-span-3">
-                    <div class="flex flex-col h-full">
-                        <div class="flex-1">
-                            <h5 class="text-gray-800 font-medium mb-1 mt-3 lg:mt-0">{{ $content->title }}</h5>
-                            <p class="text-sm">
-                                {{ Str::limit($content->description, 100) }}
-                            </p>
+        <div class="grid grid-cols-1 gap-y-3 sm:gap-x-3 lg:gap-x-5 md:grid-cols-2 xl:grid-cols-3">
+            @foreach ($contents as $content)
+                <div wire:key="video-{{ $content->id }}" class="grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-5 lg:gap-x-3 border-2 border-gray-300 rounded p-4">
+                    <div class="col-span-full lg:col-span-2 relative bg-gray-800 cursor-pointer" wire:click="openShowVideoModal({{ $content->id }})">
+                        <iframe class="absolute inset-0 w-full h-full pointer-events-none" src="{{ $this->makeVideoUrl($content) }}" frameborder="0"></iframe>
+                    </div>
+                    <div class="lg:col-span-3">
+                        <div class="flex flex-col h-full">
+                            <div class="flex-1">
+                                <h5 class="text-gray-800 font-medium mb-1 mt-3 lg:mt-0">{{ $content->title }}</h5>
+                                <p class="text-sm">
+                                    {{ Str::limit($content->description, 100) }}
+                                </p>
+                            </div>
+                            @if ($showActions)
+                                <div class="flex justify-end space-x-1 mt-1">
+                                    <button class="hover:bg-gray-100 focus:outline-none p-2 rounded-full" wire:click="onEdit({{ $content->id }})">
+                                        <x-svg.pencil class="w-4 h-4 fill-current text-gray-800" />
+                                    </button>
+                                    <button class="hover:bg-red-200 focus:outline-none p-2 rounded-full" wire:click="onDestroy({{ $content->id }})">
+                                        <x-svg.trash class="w-5 h-5  text-red-600 fill-current" />
+                                    </button>
+                                </div>
+                            @else
+                                <div class="flex justify-end space-x-1 mt-1">
+                                    &nbsp;
+                                </div>
+                            @endif
                         </div>
-                        @if ($showActions)
-                            <div class="flex justify-end space-x-1 mt-1">
-                                <button class="hover:bg-gray-100 focus:outline-none p-2 rounded-full" wire:click="onEdit({{ $content->id }})">
-                                    <x-svg.pencil class="w-4 h-4 fill-current text-gray-800" />
-                                </button>
-                                <button class="hover:bg-red-200 focus:outline-none p-2 rounded-full" wire:click="onDestroy({{ $content->id }})">
-                                    <x-svg.trash class="w-5 h-5  text-red-600 fill-current" />
-                                </button>
-                            </div>
-                        @else
-                            <div class="flex justify-end space-x-1 mt-1">
-                                &nbsp;
-                            </div>
-                        @endif
                     </div>
                 </div>
-            </div>
-        @endforeach
-    </div>
+            @endforeach
+        </div>
+    @endif
 
     <div x-data="{
         show: @entangle('showEditVideoModal').defer,
