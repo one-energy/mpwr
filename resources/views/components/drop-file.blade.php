da25f15ce1df1a894780e37eb776faed75212f04
Mathews Lima <mathews.lima@devsquad.com>
2021-04-13 10:04:11 -0300

    OE-185 change layout, simplify code

--- resources/views/components/drop-file.blade.php
+++ resources/views/components/drop-file.blade.php
@@ -3,8 +3,8 @@
 <div>
     <div x-data="initDropFileComponent()">
         <div class="flex flex-col flex-grow mb-3 border-gray-300  border-2 border-dashed rounded-md">
-            <div id="FileUpload" class="block w-full py-2 px-3 relative bg-white appearance-none ">
-                <input type="file" multiple accept=".pdf" name="inputFiles"
+            <div id="FileUpload" class="block w-full py-2 px-3 relative bg-white appearance-none cursor-pointer">
+                <input type="file" multiple accept="image/*,application/pdf" name="inputFiles"
                        class="absolute inset-0 z-50 m-0 p-0 w-full h-full outline-none opacity-0"
                        x-on:change="addFile($event.target.files)"
                        x-on:dragover="$el.classList.add('active')" x-on:dragleave="$el.classList.remove('active')" x-on:drop="$el.classList.remove('active')"
@@ -15,23 +15,27 @@
                     <p class="md:hidden text-gray-700 text-center">Click here to select your files.</p>
                 </div>
             </div>
-            <template x-if="files !== null">
-                <div class="grid grid-cols-6 h-auto">
-                    <template x-for="(_,index) in Array.from({ length: files.length })">
-                        <div class="flex flex-row justify-center items-center space-x-2 bg-gray-200 h-1/8 m-2 p-1 shadow-md rounded-md col-span-1">
-                            <span class="truncate font-medium text-gray-900" x-text="files[index].name">Uploading</span>
-                            <span class="text-xs text-gray-500" x-text="filesize(files[index].size)">...</span>
-                            <template x-if="!isUploaded(files[index])">
-                                <div><x-icon class="w-4" icon="x" x-on:click="removeFile(index)"></x-icon></div>
-                            </template>
-                            <template x-if="isUploaded(files[index])">
-                                <div><x-icon class="w-4" icon="check"></x-icon></div>
-                            </template>
-                        </div>
-                    </template>
-                </div>
-            </template>
         </div>
+        <template x-if="files !== null">
+            <div class="h-auto">
+                <template x-for="(file, index) in files">
+                    <div class="flex flex-start items-center space-x-2 mb-">
+                        <template x-if="!isUploaded(file)">
+                            <div class="cursor-pointer">
+                                <x-icon class="w-4 text-red-500" icon="x" x-on:click="removeFile(index)" />
+                            </div>
+                        </template>
+                        <span class="truncate font-medium text-gray-900" x-text="file.name">Uploading</span>
+                        <span class="text-xs text-gray-500" x-text="filesize(file.size)">...</span>
+                        <template x-if="isUploaded(file)">
+                            <div>
+                                <x-icon class="w-4  text-green-400" icon="check" />
+                            </div>
+                        </template>
+                    </div>
+                </template>
+            </div>
+        </template>
         <div class="mt-8 w-full text-center">
             <x-button class="w-1/4 bg-green-base" x-on:click="saveFiles">Save</x-button>
         </div>
@@ -70,38 +74,37 @@
                     this.files.splice(index, 1)
                 },
                 isUploaded(listedFile) {
-                    if(this.uploadedFiles) {
-                        return this.uploadedFiles.indexOf(listedFile.name) >= 0;
-                    }
-                    return false;
+                    return this.uploadedFiles
+                     ? this.uploadedFiles.indexOf(listedFile.name) >= 0
+                     : false;
                 },
                 saveFiles() {
-                    if(this.files.length){
-                        var formData = new FormData()
-                        this.files.forEach((file, index) => {
-                            formData.append(`files[${index}]`, file);
-                        });
-                        fetch(UPLOAD_URL, {
-                            method: 'post',
-                            headers: {
-                                'X-CSRF-TOKEN': this.token
-                            },
-                            body: formData,
-                        }).then((res) => {
-                            if (res.ok) {
-                                window.$app.alert({title:"Your files have been uploaded", color:"green"});
-                                return res.json();
-                            } else {
-                                window.$app.alert({title:"there is a problem with your upload"})
-                                return false;
-                            }
-                        }).then((fileUploadResponse) => {
-                            this.uploadedFiles = fileUploadResponse;
-                            console.log('uploaded', fileUploadResponse)
-                        });
-                    } else {
+                    if(this.files.length === 0) {
                         window.$app.alert({title:"Nothing to upload", color:"green"});
+                        return;
                     }
+
+                    var formData = new FormData()
+                    this.files.forEach((file, index) => {
+                        formData.append(`files[${index}]`, file);
+                    });
+                    fetch(UPLOAD_URL, {
+                        method: 'post',
+                        headers: {
+                            'X-CSRF-TOKEN': this.token
+                        },
+                        body: formData,
+                    }).then((res) => {
+                        if (res.ok) {
+                            window.$app.alert({title:"Your files have been uploaded", color:"green"});
+                            return res.json();
+                        }
+
+                        window.$app.alert({title:"there is a problem with your upload"})
+                        return false;
+                    }).then((fileUploadResponse) => {
+                        this.uploadedFiles = fileUploadResponse;
+                    });
                 }
             }
         }
