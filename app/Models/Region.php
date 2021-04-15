cb0815ab531a5e66d051aef587899973cc26d32a
Mathews Lima <mathews.lima@devsquad.com>
2021-04-15 14:24:00 -0300

    OE-217 add softDeletes trait

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\DB;
 
 /**
@@ -28,7 +29,7 @@ use Illuminate\Support\Facades\DB;
  */
 class Region extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'name',
