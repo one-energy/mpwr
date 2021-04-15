d7992b4a8b3580e6982fbf71454d5b1ea74cbf46
Mathews Lima <mathews.lima@devsquad.com>
2021-04-15 14:23:49 -0300

    OE-217 new migration for add softDeletes

---
+++ database/migrations/2021_04_15_171924_add_soft_delete_to_regions_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSoftDeleteToRegionsTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('regions', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('regions', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+    }
+}
