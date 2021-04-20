86c7699c444c203ea5bc36fcd71255d94c015198
Mathews Lima <mathews.lima@devsquad.com>
2021-04-20 09:38:03 -0300

    OE-204 new command to run query

---
+++ app/Console/Commands/SyncDailyNumbersWithUserOffice.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\DailyNumber;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class SyncDailyNumbersWithUserOffice extends Command
+{
+    protected $signature = 'daily-numbers:sync';
+
+    protected $description = 'Sync the daily_numbers.office_id with the user.office_id';
+
+    public function handle()
+    {
+        if (!$this->confirm('Are you really sure? This will sync daily_numbers.office_id with the user.office_id')) {
+            $this->info('Command aborted successfully!');
+
+            return 0;
+        }
+
+        DB::transaction(function () {
+            DailyNumber::with('user')->chunk(500, function ($dailyNumbers) {
+                $dailyNumbers->each(function (DailyNumber $dailyNumber) {
+                    $dailyNumber->update(['office_id' => $dailyNumber->user->office_id]);
+                });
+            });
+        });
+
+        $this->info('Command run successfully!');
+
+        return 0;
+    }
+}
