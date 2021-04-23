3088a0aa37fa5f465c250b6738023dc4498d611b
Mathews Lima <mathews.lima@devsquad.com>
2021-04-23 09:28:27 -0300

    OE-234 new class to be used as an enum

---
+++ app/Role/Role.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Role;
+
+final class Role
+{
+    public const ADMIN = 'Admin';
+
+    public const OWNER = 'Owner';
+
+    public const DEPARTMENT_MANAGER = 'Department Manager';
+
+    public const REGION_MANAGER = 'Region Manager';
+
+    public const OFFICE_MANAGER = 'Office Manager';
+
+    public const SALES_REP = 'Sales Rep';
+
+    public const SETTER = 'Setter';
+}
