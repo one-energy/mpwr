b860f9d9421b77aff0b5164eec18f54ed07853b6
Mathews Lima <mathews.lima@devsquad.com>
2021-04-29 16:15:04 -0300

    OE-234 new multiselect component and svg icons

---
+++ resources/views/components/multiselect.blade.php
@@ -0,0 +1,173 @@
+@props([
+    'options',
+    'labeledBy',
+    'trackBy',
+    'placeholder' => 'Select an option',
+    'label' => '',
+    'name' => ''
+])
+
+<select id="select" hidden {{ $attributes }} hidden multiple>
+    @foreach($options as $option)
+        <option value="{{ is_object($option) ? $option->{$trackBy} : $option[$trackBy] }}">
+            {{ is_object($option) ? $option->{$labeledBy} : $option[$labeledBy] }}
+        </option>
+    @endforeach
+</select>
+
+<div x-data="dropdown()" x-init="() => {
+    loadOptions();
+    $watch('selected', (value) => {
+        if (Array.isArray(value)) {
+            $wire.set('{{ $attributes->wire('model')->value() }}', selectedValues);
+        }
+    })
+}" class="sm:flex-1">
+    <label for="select" class="block text-sm font-medium leading-5 text-gray-700">{{ $label }}</label>
+    <input name="values" type="hidden" x-bind:value="selectedValues" x-cloak>
+    <div class="inline-block relative w-full">
+        <div class="flex flex-col items-center relative" x-cloak>
+            <div x-on:click="open" class="w-full" x-cloak>
+                <div class="my-2 p-1 flex border border-gray-300 bg-white rounded">
+                    <div class="flex flex-auto flex-wrap">
+                        <template x-for="option in selected" :key="option.key">
+                            <div class="space-x-1 flex justify-center items-center m-1 font-medium py-1.5 px-2 text-white rounded bg-green-400 border">
+                                <span
+                                    class="text-xs font-normal leading-none max-w-full flex-initial"
+                                    x-text="option.text">
+                                </span>
+                                <div class="flex flex-auto flex-row-reverse">
+                                    <div class="cursor-pointer" x-on:click.stop="remove(option)">
+                                        <x-icon icon="x" class="h-3 w-3 text-gray-700" />
+                                    </div>
+                                </div>
+                            </div>
+                        </template>
+                        <div x-show="selected.length == 0" class="flex-1">
+                            <input
+                                placeholder="{{ $placeholder }}"
+                                class="bg-transparent p-1 px-2 appearance-none outline-none h-full w-full text-gray-800"
+                                x-bind:value="selectedValues"
+                            >
+                        </div>
+                    </div>
+                    <div class="text-gray-300 w-8 py-1 pl-2 pr-1 border-l flex items-center border-gray-200">
+                        <button
+                            type="button"
+                            x-show="isOpen"
+                            x-on:click="open"
+                            class="cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none">
+                            <x-icon icon="chevron-down" class="h-4 w-4 text-gray-500" />
+                        </button>
+                        <button
+                            type="button"
+                            x-show="!isOpen"
+                            @click="close"
+                            class="cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none">
+                            <x-icon icon="chevron-up" class="h-4 w-4 text-gray-500" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full px-4">
+                <div x-show.transition.origin.top="isOpen"
+                     class="absolute shadow top-100 bg-white z-40 w-full left-0 max-h-select"
+                     x-on:click.away="close">
+                    <div class="flex flex-col w-full overflow-y-auto h-40">
+                        <template x-for="(option, index) in options" :key="option.key" class="overflow-auto">
+                            <div
+                                class="cursor-pointer w-full hover:bg-gray-100"
+                                :class="{'bg-green-400 text-white hover:bg-red-400': option.selected}"
+                                @click="select(option, index)"
+                            >
+                                <div class="flex w-full items-center p-2 pl-2 border-transparent border-l-2 relative">
+                                    <div class="w-full items-center flex justify-between">
+                                        <div class="mx-2 leading-6" x-model="option" x-text="option.text"></div>
+                                        <div x-show="option.selected">
+                                            {{-- TODO: Can add some check icon --}}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </template>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @error($name)
+        <p class="mt-2 text-sm text-red-600">
+            {{ $message }}
+        </p>
+    @enderror
+</div>
+
+@push('scripts')
+    <script>
+        function dropdown() {
+            return {
+                options: [],
+                selected: [],
+                show: false,
+                open() {
+                    this.show = true;
+                },
+                close() {
+                    this.show = false;
+                },
+                select(option, index) {
+                    if (this.options[index].selected) {
+                        this.optionsEl[index].selected = false;
+                        this.options[index].selected = false;
+                        this.selected = this.selected.filter(({ key }) => key !== option.key);
+
+                        return;
+                    }
+
+                    this.options[index].selected = true;
+                    this.optionsEl[index].selected = true;
+                    this.selected = [...this.selected, { ...option }];
+                },
+                remove(option) {
+                    const index = this.options.findIndex(({ key }) => key === option.key);
+                    const foundOption = this.options[index];
+
+                    this.options[index] = {...foundOption, selected: false}
+                    this.selected = this.selected.filter(({ key }) => key !== option.key);
+                },
+                loadOptions() {
+                    this.options = [...this.optionsEl].map((option, index) => {
+                        const key = Math.random().toString(36).substr(2, 5);
+
+                        if (option.selected) {
+                            this.selected.push({
+                                value: option.value,
+                                text: option.innerText.trim(),
+                                key,
+                                index
+                            });
+                        }
+
+                        return {
+                            value: option.value,
+                            text: option.innerText.trim(),
+                            selected: option.selected,
+                            key,
+                            index
+                        }
+                    });
+                },
+                get optionsEl() {
+                    return document.getElementById('select').options;
+                },
+                get selectedValues() {
+                    return this.selected.map(({ value }) => value)
+                },
+                get isOpen() {
+                    return this.show === true;
+                }
+            };
+        }
+    </script>
+@endpush
