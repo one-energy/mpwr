33e8edb80aa430d59be7ee38c8505e4bbce9e85b
Mathews Lima <mathews.lima@devsquad.com>
2021-04-29 09:55:07 -0300

    OE-234 on up move data to many-to-many, on down move data back to the original table

--- database/migrations/2021_04_23_140838_drop_region_manager_id_foreign_to_regions_table.php
+++ database/migrations/2021_04_23_140838_drop_region_manager_id_foreign_to_regions_table.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Role\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class DropRegionManagerIdForeignToRegionsTable extends Migration
@@ -9,17 +11,59 @@ class DropRegionManagerIdForeignToRegionsTable extends Migration
     public function up()
     {
         Schema::table('regions', function (Blueprint $table) {
-            $table->dropForeign('regions_region_manager_id_foreign');
+            $regions = DB::table('regions')
+                ->select('region_manager_id', 'id')
+                ->get();
+
+            $table->dropConstrainedForeignId('region_manager_id');
+
+            $regions->each(function ($region) {
+                $user = DB::table('users')
+                    ->where('role', Role::REGION_MANAGER)
+                    ->where('id', $region->region_manager_id)
+                    ->select('id')
+                    ->first();
+
+                if ($user === null) {
+                    return;
+                }
+
+                DB::table('user_managed_regions')->insert([
+                    'user_id'    => $user->id,
+                    'region_id'  => $region->id,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            });
         });
     }
 
     public function down()
     {
+        $regions = DB::table('user_managed_regions')
+            ->select('region_id', 'user_id')
+            ->get();
+
         Schema::table('regions', function (Blueprint $table) {
-            $table->foreign('region_manager_id')
+            $table
+                ->foreignId('region_manager_id')
+                ->nullable()
+                ->after('id')
                 ->references('id')
-                ->on('users')
-                ->cascadeOnDelete();
+                ->on('users');
+        });
+
+        $regions->each(function ($region) {
+            DB::table('regions')
+                ->where('id', $region->region_id)
+                ->update(['region_manager_id' => $region->user_id]);
+        });
+
+        $regions->each(function ($region) {
+            DB::table('user_managed_regions')
+                ->where('user_id', $region->user_id)
+                ->where('region_id', $region->region_id)
+                ->delete();
         });
     }
 }
