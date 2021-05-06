1c2f5533f6edb4d87acd626375e8589048cfacb7
Ron_Jr <ronaldoj.w2007@gmail.com>
2021-05-06 12:02:05 -0300

    OE-258 remove enium point amount and create controller and model

---
+++ app/Http/Controllers/EniumPointsCalculationBasisController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EniumPointsCalculationBasis;
+use Illuminate\Http\Request;
+
+class EniumPointsCalculationBasisController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\EniumPointsCalculationBasis  $eniumPointsCalculationBasis
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EniumPointsCalculationBasis $eniumPointsCalculationBasis)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\EniumPointsCalculationBasis  $eniumPointsCalculationBasis
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EniumPointsCalculationBasis $eniumPointsCalculationBasis)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\EniumPointsCalculationBasis  $eniumPointsCalculationBasis
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EniumPointsCalculationBasis $eniumPointsCalculationBasis)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\EniumPointsCalculationBasis  $eniumPointsCalculationBasis
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EniumPointsCalculationBasis $eniumPointsCalculationBasis)
+    {
+        //
+    }
+}
--- database/migrations/2021_05_06_145852_create_enium_points_calculation_bases_table.php
+++ database/migrations/2021_05_06_145852_create_enium_points_calculation_bases_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEniumPointsCalculationBasisTable extends Migration
+class CreateEniumPointsCalculationBasesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateEniumPointsCalculationBasisTable extends Migration
      */
     public function up()
     {
-        Schema::create('enium_points_calculation_basis', function (Blueprint $table) {
+        Schema::create('enium_points_calculation_bases', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->decimal('noble_pay_dealer_fee');
@@ -30,6 +30,6 @@ class CreateEniumPointsCalculationBasisTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('enium_points_calculation_basis');
+        Schema::dropIfExists('enium_points_calculation_bases');
     }
 }
