521e73305bc1fe89d33a9fa220606eb30584c646
Ron_Jr <ronaldoj.w2007@gmail.com>
2021-05-06 15:35:30 -0300

    OE-258 create table to user enium point level

---
+++ app/Http/Controllers/UserEniumPointLevelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserEniumPointLevel;
+use Illuminate\Http\Request;
+
+class UserEniumPointLevelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UserEniumPointLevel  $userEniumPointLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserEniumPointLevel $userEniumPointLevel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UserEniumPointLevel  $userEniumPointLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserEniumPointLevel $userEniumPointLevel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UserEniumPointLevel  $userEniumPointLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserEniumPointLevel $userEniumPointLevel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UserEniumPointLevel  $userEniumPointLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserEniumPointLevel $userEniumPointLevel)
+    {
+        //
+    }
+}
---
+++ database/factories/UserEniumPointLevelFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\UserEniumPointLevel;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserEniumPointLevelFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = UserEniumPointLevel::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
