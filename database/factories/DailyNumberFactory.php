9164e966ed383f897c5eaaa0f9c784481e2386d6
Mathews Lima <mathews.lima@devsquad.com>
2021-05-10 11:45:36 -0300

    OE-268 add new fileds in factory, use Arr::random

--- database/factories/DailyNumberFactory.php
+++ database/factories/DailyNumberFactory.php
@@ -13,18 +13,22 @@ class DailyNumberFactory extends Factory
     public function definition()
     {
         return [
-            'user_id'    => null,
-            'date'       => Arr::random([
+            'user_id'       => null,
+            'office_id'     => null,
+            'date'          => Arr::random([
                 now(),
                 now()->addDays(Arr::random(range(1, 10))),
-                now()->subMonth()
+                now()->subMonth(),
             ]),
-            'doors'      => rand(1, 100),
-            'hours'      => rand(1, 24),
-            'sets'       => rand(1, 100),
-            'sits'       => rand(1, 100),
-            'set_closes' => rand(1, 100),
-            'closes'     => rand(1, 100)
+            'doors'         => rand(1, 100),
+            'hours'         => Arr::random(range(1, 24)),
+            'sets'          => rand(1, 100),
+            'sits'          => rand(1, 100),
+            'set_closes'    => rand(1, 100),
+            'hours_worked'  => Arr::random(range(1, 24)),
+            'hours_knocked' => Arr::random(range(1, 24)),
+            'sats'          => Arr::random(range(1, 100)),
+            'closer_sits'   => Arr::random(range(1, 100)),
         ];
     }
 }
