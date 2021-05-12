7805665301fe3a6c8f982e06ef8fb9a577c231bc
ron-junior-devsquad <ronaldo.costa@devsquad.com>
2021-05-12 18:11:05 -0300

    OE-259 create model to stock points calculation bases

--- database/seeders/StockPointsCalculationBasesSeeder.php
+++ database/seeders/StockPointsCalculationBasesSeeder.php
@@ -2,13 +2,53 @@
 
 namespace Database\Seeders;
 
+use App\Models\StockPointsCalculationBases;
 use Illuminate\Database\Seeder;
 
-class StockPOintsCalculationBasesSeeder extends Seeder
+class StockPointsCalculationBasesSeeder extends Seeder
 {
 
     public function run()
     {
-        //
+        StockPointsCalculationBases::factory()->create([
+            'id'               => 1,
+            'name'             => 'Recruit',
+            'stock_base_point' => 25
+        ]);
+        StockPointsCalculationBases::factory()->create([
+            'id'               => 2,
+            'name'             => 'Setting',
+            'stock_base_point' => 100
+        ]);
+        StockPointsCalculationBases::factory()->create([
+            'id'               => 3,
+            'name'             => 'Personal Sales',
+            'stock_base_point' => 250
+        ]);
+        StockPointsCalculationBases::factory()->create([
+            'id'               => 4,
+            'name'             => 'Pod Leader Team',
+            'stock_base_point' => 35
+        ]);
+        StockPointsCalculationBases::factory()->create([
+            'id'               => 5,
+            'name'             => 'Manager',
+            'stock_base_point' => 50
+        ]);
+        StockPointsCalculationBases::factory()->create([
+            'id'               => 6,
+            'name'             => 'Divisional',
+            'stock_base_point' => 50
+        ]);
+        StockPointsCalculationBases::factory()->create([
+            'id'               => 7,
+            'name'             => 'Regional',
+            'stock_base_point' => 35
+        ]);
+        StockPointsCalculationBases::factory()->create([
+            'id'               => 8,
+            'name'             => 'VP',
+            'stock_base_point' => 20
+        ]);
     }
 }
