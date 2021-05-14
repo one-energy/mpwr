bb2f04b1aa147079e5a0de3ac75edc185406fb9e
ron-junior-devsquad <ronaldo.costa@devsquad.com>
2021-05-14 10:18:43 -0300

    OE-259 create test to validate se sum of stock points its all right

--- tests/Feature/Profile/StockPointsTest.php
+++ tests/Feature/Profile/StockPointsTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Profile;
 
 use App\Models\Customer;
 use App\Models\CustomersStockPoint;
+use App\Models\MultiplierOfYear;
 use App\Models\Term;
 use App\Models\User;
 use App\Models\UserCustomersEniumPoints;
@@ -17,18 +18,25 @@ use Tests\TestCase;
 
 class StockPointsTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseTransactions;
 
     public User $user;
     public Customer $customer;
+    public float $multiplier;
     public Collection $userPersonalStockPoints;
+    public Collection $teamStockPoints;
 
     protected function setUp ():void
     {
         parent::setUp();
+
         $this->userPersonalStockPoints = collect([]);
+        $this->teamStockPoints = collect([]);
+
         $this->user = User::factory()->create(['role' => 'Sales Rep']);
         $this->actingAs($this->user);
+
+        $this->multiplier = MultiplierOfYear::factory()->create(['year' => 2021])->multiplier;
 
         $this->customers = Customer::factory()->count(2)->create([
             'sales_rep_id' => $this->user->id,
@@ -39,14 +47,53 @@ class StockPointsTest extends TestCase
         $this->customers->each(function ($customer) {
             $this->userPersonalStockPoints->push(CustomersStockPoint::factory()->create(['customer_id' => $customer->id]));
         });
+
+        $this->teamStockPoints->push(Customer::factory()->has(CustomersStockPoint::factory(),'stockPoint')
+            ->create([
+                'setter_id' => $this->user->id,
+                'is_active'    => true,
+                'panel_sold'   => true
+            ])->stockPoint
+        );
+
+        $this->teamStockPoints->push(Customer::factory()->has(CustomersStockPoint::factory(),'stockPoint')
+            ->create([
+                'sales_rep_recruiter_id' => $this->user->id,
+                'is_active'              => true,
+                'panel_sold'             => true
+            ])->stockPoint
+        );
+
+        $this->teamStockPoints->push(Customer::factory()->has(CustomersStockPoint::factory(),'stockPoint')
+            ->create([
+                'office_manager_id' => $this->user->id,
+                'is_active'         => true,
+                'panel_sold'        => true
+            ])->stockPoint
+        );
+
+        $this->teamStockPoints->push(Customer::factory()->has(CustomersStockPoint::factory(),'stockPoint')
+            ->create([
+                'region_manager_id' => $this->user->id,
+                'is_active'         => true,
+                'panel_sold'        => true
+            ])->stockPoint
+        );
+
+        $this->teamStockPoints->push(Customer::factory()->has(CustomersStockPoint::factory(),'stockPoint')
+            ->create([
+                'department_manager_id' => $this->user->id,
+                'is_active'         => true,
+                'panel_sold'        => true
+            ])->stockPoint
+        );
+        
     }
 
     /** @test */
     public function it_should_show_stock_points_card()
     {
-        $response = $this->get('/');
-
-        $response->assertSee('STOCK SHARES');
+        $this->get('/')->assertSee('STOCK SHARES');
     }
 
     /** @test */
@@ -54,19 +101,42 @@ class StockPointsTest extends TestCase
     {
         $this->get('/')->assertSee($this->userPersonalStockPoints->sum('stock_personal_sale'));
     }
-
+    
     /** @test */
     public function it_should_show_team_stock_points()
     {
+        $this->get('/')->assertSee(
+            $this->teamStockPoints[0]->stock_setting + 
+            $this->teamStockPoints[1]->stock_recruiter + 
+            $this->teamStockPoints[2]->stock_manager +
+            $this->teamStockPoints[3]->stock_regional +
+            $this->teamStockPoints[4]->stock_department
+        );
     }
 
     /** @test */
     public function it_should_show_total_stock_points()
     {
+        $this->get('/')->assertSee(
+            $this->userPersonalStockPoints->sum('stock_personal_sale') +
+            $this->teamStockPoints[0]->stock_setting + 
+            $this->teamStockPoints[1]->stock_recruiter + 
+            $this->teamStockPoints[2]->stock_manager +
+            $this->teamStockPoints[3]->stock_regional +
+            $this->teamStockPoints[4]->stock_department
+        );
     }
 
     /** @test */
     public function it_should_show_gross_stock_points()
     {
+        $this->get('/')->assertSee(
+            ($this->userPersonalStockPoints->sum('stock_personal_sale') +
+            $this->teamStockPoints[0]->stock_setting + 
+            $this->teamStockPoints[1]->stock_recruiter + 
+            $this->teamStockPoints[2]->stock_manager +
+            $this->teamStockPoints[3]->stock_regional +
+            $this->teamStockPoints[4]->stock_department) * $this->multiplier
+        );
     }
 }
