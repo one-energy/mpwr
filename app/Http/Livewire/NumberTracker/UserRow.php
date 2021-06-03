5691504d602c54b055725210f98b42e30f7b8228
Mathews Lima <mathews.lima@devsquad.com>
2021-06-03 13:46:55 -0300

    OE-380 🔨 replace to array

--- app/Http/Livewire/NumberTracker/UserRow.php
+++ app/Http/Livewire/NumberTracker/UserRow.php
@@ -36,7 +36,7 @@ class UserRow extends Component
 
     public function officeSelected(int $officeId, bool $selected)
     {
-        if ($this->userDailyNumbers[0]->office_id === $officeId) {
+        if ($this->userDailyNumbers[0]['office_id'] === $officeId) {
             $this->isSelected = $selected;
         }
     }
