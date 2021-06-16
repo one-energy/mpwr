41c6c33049de8f0de6e9fcbf48bc7aac2ae119a3
ron-junior-devsquad <ronaldo.costa@devsquad.com>
2021-06-16 10:40:57 -0300

    OE-419 remove blank line

--- resources/views/livewire/number-tracker/number-tracker-detail.blade.php
+++ resources/views/livewire/number-tracker/number-tracker-detail.blade.php
@@ -137,7 +137,6 @@
                 </div>
             </div>
 
-            
             <div class="px-4 py-5 sm:p-6 md:w-2/3 xl:w-3/4">
                 <div class="justify-center w-full">
                     <livewire:number-tracker.number-tracker-detail-accordion-table :period="$period" :selectedDate="$dateSelected" wire:key="now()"/>
