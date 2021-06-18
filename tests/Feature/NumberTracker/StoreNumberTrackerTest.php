f689cca194c43f4a1577263a6b2bb2a07c1a95e3
ron-junior-devsquad <ronaldo.costa@devsquad.com>
2021-06-18 10:21:13 -0300

    OE-439 add refreshdatabase

--- tests/Feature/NumberTracker/StoreNumberTrackerTest.php
+++ tests/Feature/NumberTracker/StoreNumberTrackerTest.php
@@ -4,13 +4,12 @@ namespace Tests\Feature\NumberTracker;
 
 use App\Models\DailyNumber;
 use App\Models\User;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class StoreNumberTrackerTest extends TestCase
 {
-    use DatabaseTransactions;
+    use RefreshDatabase;
 
     private User $admin;
 
