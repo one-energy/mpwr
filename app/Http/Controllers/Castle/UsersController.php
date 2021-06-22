fa4c04b9b8352a428086993c4ccb3e24ccda9699
Mathews Lima <mathews.lima@devsquad.com>
2021-06-22 15:39:08 -0300

    OE-234 🔨 improve validation and condition - use create method from Eloquent - change queries

--- app/Http/Controllers/Castle/UsersController.php
+++ app/Http/Controllers/Castle/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Castle;
 
+use App\Enum\Role;
 use App\Http\Controllers\Controller;
 use App\Models\Department;
 use App\Models\Invitation;
@@ -40,11 +41,18 @@ class UsersController extends Controller
         $data = request()->validate([
             'first_name'    => ['required', 'string', 'max:255'],
             'last_name'     => ['required', 'string', 'max:255'],
-            'role'          => ['nullable', 'string', 'max:255'],
-            'office_id'     => ['nullable'],
+            'role'          => ['nullable', 'string', 'max:255', 'in:' . implode(',', User::getRoleByNames())],
+            'office_id'     => ['nullable', 'exists:offices,id'],
             'pay'           => ['nullable'],
-            'department_id' => ['nullable'],
-            'email'         => ['required', 'email', 'unique:invitations', new MasterEmailUnique, new MasterEmailYourSelf, 'unique:users,email'],
+            'department_id' => ['nullable', 'exists:departments,id'],
+            'email'         => [
+                'required',
+                'email:rfc,filter',
+                'unique:invitations',
+                new MasterEmailUnique,
+                new MasterEmailYourSelf,
+                'unique:users,email',
+            ],
         ], [
             'email.unique' => __('There is a pending invitation for this email.'),
         ]);
@@ -55,13 +63,13 @@ class UsersController extends Controller
         $invitation->email = $data['email'];
         $invitation->token = Uuid::uuid4();
 
-        if ($data['office_id'] == 'None') {
+        if ($data['office_id'] === 'None') {
             $data['office_id'] = null;
         }
-        if ($data['department_id'] == 'None') {
+        if ($data['department_id'] === 'None') {
             $data['department_id'] = null;
         }
-        if ($data['role'] == 'Admin' || $data['role'] == 'Owner') {
+        if (in_array($data['role'], [Role::ADMIN, Role::OWNER], true)) {
             $invitation->master    = true;
             $data['department_id'] = null;
         } else {
@@ -94,7 +102,7 @@ class UsersController extends Controller
 
     public function destroy($id)
     {
-        if ($id == auth()->user()->id) {
+        if ($id === auth()->user()->id) {
             alert()
                 ->withTitle(__('You cannot delete yourself!'))
                 ->send();
@@ -152,21 +160,18 @@ class UsersController extends Controller
 
     private function createUser(array $data, Invitation $invitation): User
     {
-        $user                    = new User();
-        $user->first_name        = $data['first_name'];
-        $user->last_name         = $data['last_name'];
-        $user->email             = $data['email'];
-        $user->password          = bcrypt(Str::random(8));
-        $user->email_verified_at = null;
-        $user->master            = $invitation->master;
-        $user->role              = $data['role'];
-        $user->office_id         = $data['office_id'];
-        $user->department_id     = $data['department_id'];
-        $user->pay               = $data['pay'];
-        $user->photo_url         = asset('storage/profiles/profile.png');
-        $user->save();
-
-        return $user;
+        return User::create([
+            'first_name'    => $data['first_name'],
+            'last_name'     => $data['last_name'],
+            'email'         => $data['email'],
+            'password'      => bcrypt(Str::random(8)),
+            'master'        => $invitation->master,
+            'role'          => $data['role'],
+            'office_id'     => $data['office_id'],
+            'department_id' => $data['department_id'],
+            'pay'           => $data['pay'],
+            'photo_url'     => asset('storage/profiles/profile.png'),
+        ]);
     }
 
     public function requestResetPassword(User $user)
@@ -222,8 +227,9 @@ class UsersController extends Controller
 
     public function getRegionsManager($departmentId)
     {
-        return User::whereDepartmentId($departmentId)
-            ->whereRole('Region Manager')
+        return User::query()
+            ->whereRole(Role::REGION_MANAGER)
+            ->whereDepartmentId($departmentId)
             ->orderBy('first_name', 'ASC')
             ->orderBy('last_name', 'ASC')
             ->get();
@@ -231,10 +237,9 @@ class UsersController extends Controller
 
     public function getOfficesManager(Region $region)
     {
-        $usersQuery = User::query()->select('users.*');
-
-        return $usersQuery->whereRole('Office Manager')
-            ->whereDepartmentId($region->department_id)
+        return User::query()
+            ->whereRole(Role::OFFICE_MANAGER)
+            ->whereDepartmentId($region?->department_id)
             ->orderBy('first_name', 'ASC')
             ->orderBy('last_name', 'ASC')
             ->get();
