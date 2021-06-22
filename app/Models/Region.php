ad1df14b7da0ddef347345ed53cc0bb07e86bb33
Mathews Lima <mathews.lima@devsquad.com>
2021-06-22 15:21:47 -0300

    OE-234 add new relationship, update docblock

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\DB;
  * @property int|null $department_id
  * @property-read \App\Models\Department|null $department
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Office[] $offices
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\User[] $managers
  * @property-read \App\Models\User $regionManager
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\TrainingPageSection[] $trainingPageSections
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Region newModelQuery()
@@ -33,13 +34,12 @@ class Region extends Model
 
     protected $fillable = [
         'name',
-        'region_manager_id',
         'department_id',
     ];
 
-    public function regionManager()
+    public function managers()
     {
-        return $this->belongsTo(User::class, 'region_manager_id');
+        return $this->belongsToMany(User::class, 'user_managed_regions')->withTimestamps();
     }
 
     public function department()
