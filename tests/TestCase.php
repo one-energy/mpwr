27fb53373c26fed6bf92924fdbe3ff105f683257
Mathews Lima <mathews.lima@devsquad.com>
2021-06-22 17:07:44 -0300

    OE-234 🔨 add new helper to create a department manager

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,24 @@
 
 namespace Tests;
 
+use App\Enum\Role;
+use App\Models\Department;
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function createVP(): mixed
+    {
+        $departmentManager = User::factory()->create(['role' => Role::DEPARTMENT_MANAGER]);
+
+        /** @var Department $department */
+        $department = Department::factory()->create();
+        $department->managers()->attach($departmentManager->id);
+        $departmentManager->update(['department_id' => $department->id]);
+
+        return [$departmentManager, $department];
+    }
 }
