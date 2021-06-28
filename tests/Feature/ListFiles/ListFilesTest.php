471e5c0eaf2c6730bf40cb455e0f19a794d89956
Mathews Lima <mathews.lima@devsquad.com>
2021-06-28 11:34:51 -0300

    OE-483 🔨 format code

--- tests/Feature/ListFiles/ListFilesTest.php
+++ tests/Feature/ListFiles/ListFilesTest.php
@@ -7,7 +7,6 @@ use App\Http\Livewire\ListFiles;
 use App\Models\Department;
 use App\Models\TrainingPageSection;
 use App\Models\User;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Collection;
@@ -17,8 +16,8 @@ use Tests\TestCase;
 
 class ListFilesTest extends TestCase
 {
-    use DatabaseTransactions;
-    
+    use RefreshDatabase;
+
     public User $user;
 
     public Department $department;
@@ -27,22 +26,21 @@ class ListFilesTest extends TestCase
 
     public Collection $files;
 
-    protected function setUp ():void
+    protected function setUp(): void
     {
-        
         parent::setUp();
 
         $this->files = collect([]);
 
         $this->department = Department::factory()->create();
 
-        $this->user       = User::factory()->create(['role' => Role::DEPARTMENT_MANAGER]);
-       
+        $this->user = User::factory()->create(['role' => Role::DEPARTMENT_MANAGER]);
+
         $this->section = TrainingPageSection::factory()->create([
-            'department_id' => $this->department->id
+            'department_id' => $this->department->id,
         ]);
- 
-        $this->actingAs($this->user);     
+
+        $this->actingAs($this->user);
 
         Storage::fake('local');
 
@@ -51,10 +49,9 @@ class ListFilesTest extends TestCase
         $this->post(route('uploadSectionFile', $this->section->id), [
             'files' => $this->files,
             'meta'  => [
-                'training_type' => 'training'
-            ]
+                'training_type' => 'training',
+            ],
         ]);
-       
     }
 
     /** @test */
@@ -62,23 +59,22 @@ class ListFilesTest extends TestCase
     {
         Livewire::test(ListFiles::class, [
             'files'            => $this->section->files,
-            'showDeleteButton' => false
+            'showDeleteButton' => false,
         ])
-        ->assertSee('avatar');
+            ->assertSee('avatar');
     }
 
     /** @test */
     public function it_should_delete_files()
     {
-
         Livewire::test(ListFiles::class, [
             'files'            => $this->section->files,
-            'showDeleteButton' => true
+            'showDeleteButton' => true,
         ])
-        ->assertSee('avatar')
-        ->call('onDestroy', $this->section->files()->first())
-        ->call('removeFile')
-        ->assertDontSee('avatar');
+            ->assertSee('avatar')
+            ->call('onDestroy', $this->section->files()->first())
+            ->call('removeFile')
+            ->assertDontSee('avatar');
     }
 
     /** @test */
@@ -86,20 +82,19 @@ class ListFilesTest extends TestCase
     {
         Livewire::test(ListFiles::class, [
             'files'            => $this->section->files,
-            'showDeleteButton' => false
+            'showDeleteButton' => false,
         ])
-        ->assertDontSeeHtml('<x-svg.trash class="w-5 h-5  text-red-600 fill-current" />');
+            ->assertDontSeeHtml('<x-svg.trash class="w-5 h-5  text-red-600 fill-current" />');
     }
 
     /** @test */
     public function it_should_download_file()
     {
-        
         Livewire::test(ListFiles::class, [
             'files'            => $this->section->files,
-            'showDeleteButton' => false
+            'showDeleteButton' => false,
         ])
-        ->call('downloadSectionFile', $this->section->files->first())
-        ->assertDispatchedBrowserEvent('show-alert');
+            ->call('downloadSectionFile', $this->section->files->first())
+            ->assertDispatchedBrowserEvent('show-alert');
     }
 }
