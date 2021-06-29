b3ec0dc56e7e56537804f0b0b889d61db559759d
ron-junior-devsquad <ronaldo.costa@devsquad.com>
2021-06-29 14:01:24 -0300

    OE-495 remove interpolation

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,26 +1,23 @@
-@props(['type', 'color', 'href'])
+@props(['type', 'color' => "gray", 'href'])
 
 @php
-    $color = $color ?? 'green';
     $href = $href ?? null;
+    $class = " border border-green-base text-white bg-green-base hover:bg-green-dark hover:border-green-dark focus:border-green-500 focus:shadow-outline-green active:bg-green-50";
+    if($color == "gray")  {
+        $class = "border border-transparent bg-gray-600 hover:bg-gray-500 focus:outline-none focus:border-gray-700 focus:shadow-outline-gray active:bg-gray-700";
+    }
+    
 @endphp
 
 <span class="rounded-md shadow-sm">
     @if($href)
         <a href="{{ $href }}"
-            {{ $attributes->merge(['class' => "justify-center py-2 px-4
-            @if($color == 'green') border border-green-base text-white bg-green-base hover:border-green-dark hover:bg-green-dark focus:border-green-500 focus:shadow-outline-green active:bg-green-50 
-            @else border border-transparent text-white bg-{$color}-600 hover:bg-{$color}-500 focus:border-{$color}-700 focus:shadow-outline-{$color} active:bg-{$color}-700 
-            @endif text-sm font-medium rounded-md focus:outline-none transition duration-150 ease-in-out"]) }}
-        >
+            {{ $attributes->merge(['class' => "justify-center py-2 px-4 text-sm font-medium rounded-md focus:outline-none transition duration-150 ease-in-out" . $class]) }}>
             {{ $slot }}
         </a>
     @else
         <button type="{{ $type ?? 'button' }}"
-            {{ $attributes->merge(['class' => "justify-center py-2 px-4
-            @if($color == 'green') border border-green-base text-white bg-green-base hover:bg-green-dark hover:border-green-dark focus:border-green-500 focus:shadow-outline-green active:bg-green-50 
-            @else border border-transparent bg-{$color}-600 hover:bg-{$color}-500 focus:outline-none focus:border-{$color}-700 focus:shadow-outline-{$color} active:bg-{$color}-700 
-            @endif text-sm font-medium rounded-md text-white transition duration-150 ease-in-out"]) }}
+            {{ $attributes->merge(['class' => "justify-center py-2 px-4 text-sm font-medium rounded-md text-white transition duration-150 ease-in-out" . $class]) }}
         >
             {{ $slot }}
         </button>
