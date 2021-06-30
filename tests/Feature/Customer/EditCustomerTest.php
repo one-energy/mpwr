64201e4790362ded27214261d0f92f2b8d874d6d
ron-junior-devsquad <ronaldo.costa@devsquad.com>
2021-06-30 16:45:57 -0300

    OE-510 add test to validate if was created stock point when install panel

---
+++ tests/Feature/Customer/EditCustomerTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Tests\Feature\Customer;
+
+use App\Enum\Role;
+use App\Http\Livewire\Customer\Edit;
+use App\Models\Customer;
+use App\Models\CustomersStockPoint;
+use App\Models\Department;
+use App\Models\MultiplierOfYear;
+use App\Models\StockPointsCalculationBases;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Collection;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class EditCustomerTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public User $departmentManager;
+    public User $regionManager;
+    public User $officeManager;
+    public User $salesRep;
+    public User $setter;
+    public Department $department;
+    public Customer $customer;
+    public Collection $stockPointsCalculations;
+    public float $multiplier;
+    public Collection $customers;
+
+    protected function setUp ():void
+    {
+        parent::setUp();
+
+        $this->stockPointsCalculations = StockPointsCalculationBases::factory()->count(8)->create();
+
+        $this->department = Department::factory()->create();
+
+        $this->departmentManager = User::factory()->create([
+            'role' => Role::DEPARTMENT_MANAGER,
+            'department_id' => Department::factory(),
+        ]);
+
+        $this->departmentManager = User::factory()->create([
+            'role' => Role::DEPARTMENT_MANAGER,
+            'department_id' => $this->department->id
+        ]);
+        $this->regionManager     = User::factory()->create([
+            'role' => Role::REGION_MANAGER,
+            'department_id' => $this->department->id
+        ]);
+        $this->officeManager     = User::factory()->create([
+            'role' => Role::OFFICE_MANAGER,
+            'department_id' => $this->department->id
+        ]);
+        $this->setter            = User::factory()->create([
+            'role' => Role::SETTER,
+            'department_id' => $this->department->id
+        ]);
+        $this->salesRep          = User::factory()->create([
+            'role' => Role::SALES_REP,
+            'department_id'         => $this->department->id,
+            'department_manager_id' => $this->departmentManager->id,
+            'region_manager_id'     => $this->regionManager->id,
+            'office_manager_id'     => $this->officeManager->id,
+        ]);
+
+        $this->multiplier = MultiplierOfYear::factory()->create(['year' => 2021])->multiplier;
+        
+        $this->customers = Customer::factory()
+            ->count(2)
+            ->state(new Sequence([
+                'sales_rep_id' => $this->salesRep->id,
+                'sales_rep_id' => $this->officeManager->id,
+            ]))
+            ->create([
+                'is_active'    => true,
+                'panel_sold'   => true
+            ]);
+        
+        $this->actingAs($this->departmentManager);
+    }
+    
+    /** @test */
+    public function it_should_create_personal_stock_points_when_set_as_install_customer()
+    {
+        Livewire::test(Edit::class,['customer' => $this->customers->first()])
+            ->set('customer.panel_sold', true)
+            ->call('update');
+        
+        $this->assertDatabaseHas('customers_stock_points', [
+            'customer_id'         => $this->customers->first()->id,
+            'stock_personal_sale' => $this->stockPointsCalculations[StockPointsCalculationBases::PERSONAL_SALES_ID]->stock_base_point
+        ]);
+    }
+}
