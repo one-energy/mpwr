87253a1c7e20ece810d660103d069a12088ad80c
Mathews Lima <mathews.lima@devsquad.com>
2021-07-02 13:34:04 -0300

    OE-524 remove search from route

--- routes/web.php
+++ routes/web.php
@@ -131,7 +131,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::put('/customers/{customer}/active', [CustomerController::class, 'active'])->name('customers.active');
     Route::delete('/customers/{customer}', [CustomerController::class, 'delete'])->name('customers.delete');
     Route::get('/leaderboard', ScoreboardController::class)->name('leaderboard');
-    Route::get('/trainings/{department?}/{section?}/{search?}', [TrainingController::class, 'index'])->name('trainings.index');
+    Route::get('/trainings/{department?}/{section?}', [TrainingController::class, 'index'])->name('trainings.index');
     Route::get('/incentives', IncentivesController::class)->name('incentives.index');
     Route::get('/reports', [ReportsController::class, 'index'])->name('reports.index');
 
