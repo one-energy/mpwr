24538bdd33fc4a93d774a4c1505dd8924fea3dbd
Mathews Lima <mathews.lima@devsquad.com>
2021-07-02 15:27:25 -0300

    OE-526 🔒️ add new test cases

--- tests/Feature/Livewire/Castle/ManageTrainings/Trainings/GetTrainingsTest.php
+++ tests/Feature/Livewire/Castle/ManageTrainings/Trainings/GetTrainingsTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\Feature\Livewire\Castle\ManageTrainings\Trainings;
 
+use App\Enum\Role;
 use App\Http\Livewire\Castle\ManageTrainings\Trainings;
 use App\Models\Department;
 use App\Models\TrainingPageSection;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Collection;
 use Livewire\Livewire;
 use Tests\TestCase;
 
@@ -17,7 +19,7 @@ class GetTrainingsTest extends TestCase
     /** @test */
     public function it_should_be_possible_change_tabs()
     {
-        $john = User::factory()->create(['role' => 'Admin']);
+        $john = User::factory()->create(['role' => Role::ADMIN]);
 
         $department = Department::factory()->create();
         TrainingPageSection::factory()->create(['department_id' => $department->id]);
@@ -29,6 +31,44 @@ class GetTrainingsTest extends TestCase
             ->assertSet('trainingTabSelected', false)
             ->call('changeTab', 'training')
             ->assertSet('filesTabSelected', false)
-            ->assertSet('trainingTabSelected', true);
+            ->assertSet('trainingTabSelected', true)
+            ->assertHasNoErrors();
+    }
+
+    /** @test */
+    public function it_should_attach_first_department_in_db_if_auth_user_is_admin_and_no_department_provided()
+    {
+        $john = User::factory()->create(['role' => Role::ADMIN]);
+
+        /** @var Collection $departments */
+        $departments = Department::factory()->times(2)->create();
+        TrainingPageSection::factory()->create(['department_id' => $departments->first()->id]);
+
+        $this->actingAs($john);
+
+        Livewire::test(Trainings::class)
+            ->assertSet('department.id', $departments->first()->id)
+            ->assertHasNoErrors();
+    }
+
+    /** @test */
+    public function it_should_be_possible_change_department()
+    {
+        $john = User::factory()->create(['role' => Role::ADMIN]);
+
+        $department01 = Department::factory()->create();
+        $department02 = Department::factory()->create();
+        TrainingPageSection::factory()->create(['department_id' => $department01->id]);
+
+        $this->actingAs($john);
+
+        Livewire::test(Trainings::class, ['department' => $department01])
+            ->call('changeDepartment', $department02->id)
+            ->assertRedirect(route('castle.manage-trainings.index', ['department' => $department02->id]))
+            ->assertHasNoErrors();
+
+        Livewire::test(Trainings::class, ['department' => $department01])
+            ->call('changeDepartment', 99)
+            ->assertHasNoErrors();
     }
 }
